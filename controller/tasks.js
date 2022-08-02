bd1a9046b8382883acddcff76bf9176ae3273105
sahil6699 <sahilaneja66199@gmail.com>
2022-08-02 15:42:47 +0530

    getAllTasks

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,14 @@
+const task = require("../models/task")
 const Task = require("../models/task")
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks")
+const getAllTasks = async (req, res) => {
+  // res.send("get all tasks")
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks: tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createTask = async (req, res) => {
