9e631209fce187db68ebd524c4e269164db27fe4
sahil6699 <sahilaneja66199@gmail.com>
2022-08-03 03:00:11 +0530

    using put method
    diffrence b/w put and patch is that in put we replace the whole thing for our new one, whereas in patch we just update the thing that we have been given
    
    moongoose by defalut doesn't replace the whole thing for our new one so we have to supply the property:- overwrite: true
    
    in real life we 'll se put method being used as the patch method

--- controller/tasks.js
+++ controller/tasks.js
@@ -72,10 +72,31 @@ const updateTask = async (req, res) => {
   }
 }
 
+const editTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+      overwrite: true,
+    })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
 module.exports = {
   getAllTasks,
   createTask,
   getTask,
   updateTask,
   deleteTask,
+  editTask,
 }
