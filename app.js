57207f4059b674d8e0cfb7da7fc398f6c0bcf475
sahil6699 <sahilaneja66199@gmail.com>
2022-08-04 16:07:19 +0530

    custon errror handler for our app
    generally for async operations we have to pass the error on to the next for the express to come in play and do the error handling for us, but we can set up our own custom error handlers, here we have used the middleware and exported and then imported the error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 require("dotenv").config()
 const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 
 //middleware
 app.use(express.static("./public"))
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use("/api/v1/tasks", tasks)
 
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async () => {
   try {
